2ecb31fe343644e4aca8a6f6a3cdcd430b6d9243
Tom Yaxley <tommitytom@gmail.com>
2019-12-03 16:43:21 +1100

    Fix yml

--- vsver.bat
+++ vsver.bat
@@ -1,4 +1,5 @@
 @echo off
+setlocal ENABLEDELAYEDEXPANSION
 
 FOR /F "tokens=* USEBACKQ" %%F IN (`vswhere.exe -property catalog_productLineVersion -all`) DO ( SET VS_VERSION=%%F )
 for /l %%a in (1,1,128) do if "!VS_VERSION:~-1!"==" " set VS_VERSION=!VS_VERSION:~0,-1!
